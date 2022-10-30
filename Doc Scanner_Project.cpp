96026a4941c3a786f9aa259bd5141c43dbbe7979
Indominous1 <93007035+Indominous1@users.noreply.github.com>
2022-10-30 20:45:34 +0530

    Update Doc Scanner_Project.cpp

--- Doc Scanner_Project.cpp
+++ Doc Scanner_Project.cpp
@@ -1,27 +1,29 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-#include<iostream>
+#include <iostream>
 
-using namespace std;
 using namespace cv;
+using namespace std;
 
-Mat imgoriginal, imgGray, imgBlur, imgCanny,imgthres,imgDil, imgErode;
-vector<Point>initialPoints;
+//////////////////////////////  Project - Document Scanner  /////////////////////////////////////////
+
+Mat imgOriginal, imgGray, imgBlur, imgCanny, imgThre, imgDil, imgErode, imgWarp, imgCrop;
+vector<Point> initialPoints, docPoints;
+float w = 420, h = 596;
 
 Mat preProcessing(Mat img)
 {
 	cvtColor(img, imgGray, COLOR_BGR2GRAY);
-	GaussianBlur(img, imgBlur, Size(5, 3), 0, 7);
-	Canny(imgBlur, imgCanny, 50, 120);
-
-	Mat kernel = getStructuringElement(MORPH_RECT, Size(1, 1));
-
+	GaussianBlur(imgGray, imgBlur, Size(3, 3), 3, 0);
+	Canny(imgBlur, imgCanny, 25, 75);
+	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
 	dilate(imgCanny, imgDil, kernel);
-	erode(imgDil, imgErode, kernel);
-	return imgErode;
+	//erode(imgDil, imgErode, kernel);
+	return imgDil;
 }
-  vector<Point> getContours(Mat image) {
+
+vector<Point> getContours(Mat image) {
 
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
@@ -47,44 +49,85 @@ Mat preProcessing(Mat img)
 			float peri = arcLength(contours[i], true);
 			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
 
-			if (area > maxArea && conPoly[i].size()==4) {
+			if (area > maxArea && conPoly[i].size() == 4) {
 
-				biggest = {conPoly[i][0],conPoly[i][1],conPoly[i][2],conPoly[i][3] };
+				//drawContours(imgOriginal, conPoly, i, Scalar(255, 0, 255), 5);
+				biggest = { conPoly[i][0],conPoly[i][1] ,conPoly[i][2] ,conPoly[i][3] };
 				maxArea = area;
 			}
-
-			//drawContours(imgoriginal, conPoly, i, Scalar(255, 0, 255), 2);
-			//rectangle(imgoriginal, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
+			//drawContours(imgOriginal, conPoly, i, Scalar(255, 0, 255), 2);
+			//rectangle(imgOriginal, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
 		}
 	}
 	return biggest;
 }
 
-  void drawPoints(vector<Point>points, Scalar color) 
-  {
-	  for (int i = 0; i < points.size(); i++)
-	  {
-		  circle(imgoriginal, points[i], 30, color, FILLED);
-		  putText(imgoriginal, to_string(i), points[i], FONT_HERSHEY_PLAIN, 5, color, 5);
-
-	  }
-  }
-
-
-void main()
+void drawPoints(vector<Point> points, Scalar color)
 {
+	for (int i = 0; i < points.size(); i++)
+	{
+		circle(imgOriginal, points[i], 10, color, FILLED);
+		putText(imgOriginal, to_string(i), points[i], FONT_HERSHEY_PLAIN, 4, color, 4);
+	}
+}
+
+vector<Point> reorder(vector<Point> points)
+{
+	vector<Point> newPoints;
+	vector<int>  sumPoints, subPoints;
+
+	for (int i = 0; i < 4; i++)
+	{
+		sumPoints.push_back(points[i].x + points[i].y);
+		subPoints.push_back(points[i].x - points[i].y);
+	}
+
+	newPoints.push_back(points[min_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); // 0
+	newPoints.push_back(points[max_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); //1
+	newPoints.push_back(points[min_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); //2
+	newPoints.push_back(points[max_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); //3
+
+	return newPoints;
+}
+
+Mat getWarp(Mat img, vector<Point> points, float w, float h)
+{
+	Point2f src[4] = { points[0],points[1],points[2],points[3] };
+	Point2f dst[4] = { {0.0f,0.0f},{w,0.0f},{0.0f,h},{w,h} };
+
+	Mat matrix = getPerspectiveTransform(src, dst);
+	warpPerspective(img, imgWarp, matrix, Point(w, h));
+
+	return imgWarp;
+}
+
+void main() {
+
 	string path = "Resources/Documents.jpg";
-	Mat imgoriginal = imread(path);
-	resize(imgoriginal, imgoriginal, Size(), 0.40,0.40);
-	//Preprocessing//
-	imgthres = preProcessing(imgoriginal);
+	imgOriginal = imread(path);
+	//resize(imgOriginal, imgOriginal, Size(), 0.5, 0.5);
 
-	// Find Biggest Countour in the Image//
-	initialPoints=getContours(imgthres);
+	// Preprpcessing - Step 1 
+	imgThre = preProcessing(imgOriginal);
 
-	drawPoints(initialPoints, Scalar(0, 0, 255));
+	// Get Contours - Biggest  - Step 2
+	initialPoints = getContours(imgThre);
+	//drawPoints(initialPoints, Scalar(0, 0, 255));
+	docPoints = reorder(initialPoints);
+	//drawPoints(docPoints, Scalar(0, 255, 0));
 
-	imshow("Original Image", imgoriginal);
-	imshow("Image", imgthres);
+	// Warp - Step 3 
+	imgWarp = getWarp(imgOriginal, docPoints, w, h);
+
+	//Crop - Step 4
+	int cropVal = 5;
+	Rect roi(cropVal, cropVal, w - (2 * cropVal), h - (2 * cropVal));
+	imgCrop = imgWarp(roi);
+
+	imshow("Image", imgOriginal);
+	//imshow("Image Dilation", imgThre);
+	//imshow("Image Warp", imgWarp);
+	imshow("Image Crop", imgCrop);
 	waitKey(0);
+
 }
